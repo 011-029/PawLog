d8ba160b6c12847f3d5d35c5ffa472cde66e0644
Minseo <svt72858@gmail.com>
2025-11-26 11:10:46 +0900

    refactor : 쓸데없는 주석 삭제

--- src/main/java/core/VaccineMgr.java
+++ src/main/java/core/VaccineMgr.java
@@ -7,7 +7,6 @@ import mgr.Factory;
 import mgr.PetRecordMgr;
 
 // 예방접종 기록 관리 클래스
-// TODO: 클래스 필드, 메서드 작성 (현재는 필수 구현해야 하는 메서드만 자동완성해둔 상태)
 public class VaccineMgr extends PetRecordMgr<VaccineRecord> {
     private static VaccineMgr mgr = null;
     private static final String FILE_PATH = "data/vaccine_records.txt";
